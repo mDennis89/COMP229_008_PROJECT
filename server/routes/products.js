d83ff8f36a3d611cffc18db7e055a534e7556495
mDennis89 <mdemmis@my.centennialcollege.ca>
2023-07-17 20:28:35 -0400

    update codes

--- server/routes/products.js
+++ server/routes/products.js
@@ -102,18 +102,20 @@ router.post('/:id', (req, res, next) => {
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
-// Q.2e
-  let productId = req.params.id;
+  let id = req.params.id;
 
-  // get the ID and then delete it from the DB
-  product.findByIdAndDelete(productId)
-    .exec()
-    .then(() => {
-      res.redirect('/products');
-    })
-    .catch((err) => {
-      console.error('Error deleting product: ', err);
-    });
+  product.remove({_id: id}, (err) => {
+      if(err)
+      {
+          console.log(err);
+          res.end(err);
+      }
+      else
+      {
+          // refresh the Product list
+          res.redirect('/products');
+      }
+  });
 });
 
 
